cd4cc8b8591538eb140c8bcae1f0665fa034d74e
Tushar <galaxy.tusharraut@gmail.com>
2019-12-30 14:04:43 +0530

    Create resource on k8s cluster

---
+++ create_resource.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	// Create client
+	var kubeconfig string
+	kubeconfig, ok := os.LookupEnv("KUBECONFIG")
+	if !ok {
+		kubeconfig = filepath.Join(homedir.HomeDir(), ".kube", "config")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	kubeclient := clientset.CoreV1().ServiceAccounts("px-central")
+
+	// Create resource object
+	object := &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ServiceAccount",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pxcentral-onprem-operator-test",
+			Namespace: "px-central",
+		},
+	}
+
+	// Manage resource
+	_, err = kubeclient.Create(object)
+	if err != nil {
+		//panic(err)
+		_, err = kubeclient.Update(object)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("ServiceAccount Created successfully!")
+}
